4dc8e1d8bf48431f49cb8e9172ff11027a64cf55
Bayusukmoadji <147709074+Bayusukmoadji@users.noreply.github.com>
2023-11-16 18:03:25 +0700

    UTS directory

--- UTS/jawaban4.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-mahasiswa.id as SESI,
-mahasiswa.first_name as NAMA,
-mahasiswa.jurusan AS PRODI,
-mahasiswa.kode_matkul AS MATKUL,
-mata_kuliah.jumlah_sks AS SKS
-FROM mahasiswa
-INNER JOIN mata_kuliah ON mahasiswa.kode_matkul = mata_kuliah.kode_matkul;
